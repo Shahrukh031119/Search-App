592f646990b05623256a50cc5eac88fbaedd7eee
Shahrukh031119 <alliswelldep5819@gmail.com>
2022-09-18 14:25:00 +0530

    implemented faker api to fetch and display data

--- src/Components/SuggestionBox.tsx
+++ src/Components/SuggestionBox.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './SuggestionBox.scss';
+import {faker} from '@faker-js/faker';
 
 const SuggestionBox = () => {
     return(
         <div className='Result' id='property'>
-            <div className='LatestTrends'><b>Latest Trends</b></div>
-            <div className='PopularSuggestions'><b>Popular Suggestions</b></div>
+            <div className='LatestTrends'>
+                <b>Latest Trends</b>
+                <div className='LatestItems'>
+                    <img src={faker.image.fashion(110,150,true)} alt=""/>
+                    <img src={faker.image.fashion(110,150,true)} alt=""/>
+                    <img src={faker.image.fashion(110,150,true)} alt=""/>
+                    <img src={faker.image.fashion(110,150,true)} alt=""/>
+                    <img src={faker.image.fashion(110,150,true)} alt=""/>
+                </div>
+            </div>
+            <div className='PopularSuggestions'>
+                <b>Popular Suggestions</b>
+                <div className='SuggestionItems'>
+                    <div>{faker.commerce.product()}</div>
+                    <div>{faker.commerce.product()}</div>
+                    <div>{faker.commerce.product()}</div>
+                    <div>{faker.commerce.product()}</div>
+                    <div>{faker.commerce.product()}</div>
+                </div>
+            </div>
         </div>
     )
 }
